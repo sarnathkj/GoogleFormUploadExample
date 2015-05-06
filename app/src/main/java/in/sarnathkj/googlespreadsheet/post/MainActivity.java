475b39f7937642b5b4afd93cba8c190efc4fa846
Sarnath Kuduva Jegadeesan <sarnathkj@outlook.com>
2015-05-06 18:15:55 +0530

    UI-Change
    Now can upload data manually entered in the TextBox and upload to Google
    forms. Uploading happens in thread as a background task. Next commit
    will have improvements in UI and UI States.

--- app/src/main/java/in/sarnathkj/googlespreadsheet/post/MainActivity.java
+++ app/src/main/java/in/sarnathkj/googlespreadsheet/post/MainActivity.java
@@ -5,6 +5,8 @@ import java.net.URLEncoder;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 	
@@ -17,15 +19,20 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Log.i(myTag, "OnCreate()");
-		Thread t = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				postData();
-				
-			}
-		});
-		t.start();
 	}
+
+    public void callPostData(View view) {
+        Log.i(myTag, "Calling Post Data");
+        //Creating a thread since network actions handled in the background.
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                postData();
+
+            }
+        });
+        t.start();
+    }
 
 	
 
@@ -33,9 +40,14 @@ public class MainActivity extends Activity {
 
 		String fullUrl = "https://docs.google.com/forms/d/1yd8oQb1TMtNDYYwkqgV78wTu8Fd9P3OGJSa1MCJoE9U/formResponse";
 		HttpRequest mReq = new HttpRequest();
-		String col1 = "Sarnath";
+
+        //Getting Sensor Value
+        EditText sensorValue = (EditText) findViewById(R.id.sensor_value);
+
+		String finalSensorValue = sensorValue.getText().toString();
 		
-		String data = "entry.75015930=" + URLEncoder.encode(col1);
+		String data = "entry.75015930=" + URLEncoder.encode(finalSensorValue);
+        Log.i(myTag, "TestData-"+data);
 		String response = mReq.sendPost(fullUrl, data);
 		Log.i(myTag, response);
 	} 
